6784d96a33dd0b2a52dbb9368cf6796d18bd2cf7
choudhariprathmesh <prathmeshchoudhari07@gmail.com>
2023-01-18 12:37:46 +0530

    code edited
    code edited

--- 14_SortingAscendingAndDescending.js
+++ 14_SortingAscendingAndDescending.js
@@ -41,3 +41,15 @@ empArray.forEach((value)=>{
 })
 
 console.warn("hello World")
+
+
+ function find_max(nums) {
+     let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+     for (let num of nums) {
+     if (num > max_num) {
+     // (Fill in the missing line here)
+     }
+     }
+     return max_num;
+     }
+    
